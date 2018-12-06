241dbe46243ecebcebf2aa22bcb1a0788aeaac98
Maxime Riggi <maxime.riggi@iliium.com>
2018-12-06 15:47:04 +0100

    add bootkey

--- getbootnodeurl.sh
+++ getbootnodeurl.sh
@@ -7,5 +7,5 @@ DATA_ROOT=${DATA_ROOT:-$(pwd)}
 MYIP=$(docker exec ethereum-bootnode ifconfig eth0 | awk '/inet addr/{print substr($2,6)}')
 #ENODE_LINE=$(echo $ENODE_LINE | sed "s/127\.0\.0\.1/$MYIP/g" | sed "s/\[\:\:\]/$MYIP/g")
 #echo "enode:${ENODE_LINE#*enode:}"
-ENODEKEY = $(docker run --rm $IMGNAME bootnode --nodekey $DATA_ROOT/.bootnode -writeaddress)
+ENODEKEY = $(docker run --rm $IMGNAME bootnode --nodekey $DATA_ROOT/.bootnode/boot.key -writeaddress)
 echo "enode://$ENODEKEY@$MYIP:30301"
