f7849e74aa8f5adefbf6767d1621b8bc10565b6a
Maxime Riggi <maxime.riggi@iliium.com>
2020-01-06 16:51:19 +0100

    fix shell error ...

--- runpoolminer.sh
+++ runpoolminer.sh
@@ -3,5 +3,4 @@ NODE_NAME=$1
 NODE_NAME=${NODE_NAME:-"miner1"}
 ETHERBASE=${ETHERBASE:-"0x0000000000000000000000000000000000000001"}
 # expose rpc port
-RPC_PORT=8545
-./runnode.sh $NODE_NAME --mine --miner.threads=0 --miner.extradata="$NODE_NAME" --etherbase="$ETHERBASE"
+RPC_PORT=8545 ./runnode.sh $NODE_NAME --mine --miner.threads=0 --miner.extradata="$NODE_NAME" --etherbase="$ETHERBASE"
