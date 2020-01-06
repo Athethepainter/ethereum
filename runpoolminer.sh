1666ac648a31320b7142e65aa46cbb1cb019f6fe
Maxime Riggi <maxime.riggi@iliium.com>
2020-01-06 16:22:52 +0100

    fix usb devices and fast sync error, reduce log level

--- runpoolminer.sh
+++ runpoolminer.sh
@@ -2,4 +2,4 @@
 NODE_NAME=$1
 NODE_NAME=${NODE_NAME:-"miner1"}
 ETHERBASE=${ETHERBASE:-"0x0000000000000000000000000000000000000001"}
-./runnode.sh $NODE_NAME --mine --miner.threads=1 --miner.extradata="$NODE_NAME" --etherbase="$ETHERBASE"
+./runnode.sh $NODE_NAME --mine --miner.threads=0 --miner.extradata="$NODE_NAME" --etherbase="$ETHERBASE"
